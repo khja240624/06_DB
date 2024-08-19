fba8804f59b73c3d9777d49c01d2d0c2f5a5b99e
khja240624 <khja240624@gmail.com>
2024-08-19 17:32:03 +0900

    20240819_단일행 서브쿼리

--- General/Scripts/05_SUBQUERY.sql
+++ General/Scripts/05_SUBQUERY.sql
@@ -2,7 +2,7 @@
     * SUBQUERY (서브쿼리)
     - 하나의 SQL문 안에 포함된 또다른 SQL문
     - 메인쿼리(기존쿼리)를 위해 보조 역할을 하는 쿼리문
-    -- SELECT, FROM, WHERE, HAVGIN 절에서 사용가능
+    -- SELECT, FROM, WHERE, HAVING 절에서 사용가능
 
 */  
 
@@ -11,13 +11,27 @@
 -- 이름, 부서코드 조회하기
 
 -- 1) 사원명이 노옹철인 사람의 부서코드 조회
+SELECT DEPT_CODE
+FROM EMPLOYEE
+WHERE EMP_NAME = '노옹철'; -- 'D9'
 
 
 -- 2) 부서코드가 D9인 직원을 조회
+SELECT EMP_NAME, DEPT_CODE
+FROM EMPLOYEE
+WHERE DEPT_CODE = 'D9';
+
 
 
 -- 3) 부서코드가 노옹철사원과 같은 소속의 직원 명단 조회   
 --> 위의 2개의 단계를 하나의 쿼리로!!! --> 1) 쿼리문을 서브쿼리로!!
+SELECT EMP_NAME, DEPT_CODE
+FROM EMPLOYEE
+WHERE DEPT_CODE = 
+	(SELECT DEPT_CODE
+	 FROM EMPLOYEE
+	 WHERE EMP_NAME = '노옹철');
+
 
                    
                    
@@ -27,24 +41,39 @@
 -- 사번, 이름, 직급코드, 급여 조회
 
 -- 1) 전 직원의 평균 급여 조회하기
-
+SELECT FLOOR(AVG(SALARY)) 
+FROM EMPLOYEE;
 
 -- 2) 직원들중 급여가 4091140원 이상인 사원들의 사번, 이름, 직급코드, 급여 조회
-
+SELECT EMP_ID, EMP_NAME, JOB_CODE, SALARY
+FROM EMPLOYEE
+WHERE SALARY >= 4091140;
 
 -- 3) 전 직원의 평균 급여보다 많은 급여를 받고 있는 직원 조회
 --> 위의 2단계를 하나의 쿼리로 가능하다!! --> 1) 쿼리문을 서브쿼리로!!
+SELECT EMP_ID, EMP_NAME, JOB_CODE, SALARY
+FROM EMPLOYEE
+WHERE SALARY >= 
+	(SELECT FLOOR(AVG(SALARY)) 
+	FROM EMPLOYEE);
 
-                 
+     
+/***********************************************/
+-- 기본적으로 "서브 쿼리"를 먼저 해석!!!!
+ --> 서브쿼리 결과를 이용해서 "메인 쿼리" 해석!!!
+
+-- 단, 상호연관 서브쿼리는 순서가 반대!!(메인 -> 서브)
+
+/***********************************************/
 
 
 -------------------------------------------------------------------
 
 /*  서브쿼리 유형
 
-    - 단일행 서브쿼리 : 서브쿼리의 조회 결과 값의 개수가 1개일 때 
+    - 단일행 (단일열) 서브쿼리 : 서브쿼리의 조회 결과 값의 개수가 1개일 때 
     
-    - 다중행 서브쿼리 : 서브쿼리의 조회 결과 값의 개수가 여러개일 때
+    - 다중행 (단일열) 서브쿼리 : 서브쿼리의 조회 결과 값의 개수가 여러개일 때
     
     - 다중열 서브쿼리 : 서브쿼리의 SELECT 절에 자열된 항목수가 여러개 일 때
     
@@ -62,39 +91,130 @@
 
 -- 1. 단일행 서브쿼리 (SINGLE ROW SUBQUERY)
 --    서브쿼리의 조회 결과 값의 개수가 1개인 서브쿼리
---    단일행 서브쿼리 앞에는 비교 연산자 사용
+--    WHERE절에서 단일행 서브쿼리 앞에는 비교 연산자 사용
 --    <, >, <=, >=, =, !=/^=/<>
 
 
 -- 전 직원의 급여 평균보다 많은 급여를 받는 직원의 
--- 이름, 직급, 부서, 급여를 직급 순으로 정렬하여 조회
+-- 이름, 직급명, 부서명, 급여를 직급 순으로 정렬하여 조회
+SELECT EMP_NAME, JOB_NAME, DEPT_TITLE, SALARY
+FROM EMPLOYEE
+JOIN JOB USING(JOB_CODE)
+LEFT JOIN DEPARTMENT ON(DEPT_CODE = DEPT_ID)
+WHERE SALARY > 
+	(SELECT AVG(SALARY)
+	 FROM EMPLOYEE)
+ORDER BY JOB_CODE ASC;
 
 
 
 -- 가장 적은 급여를 받는 직원의
--- 사번, 이름, 직급, 부서코드, 급여, 입사일을 조회
+-- 사번, 이름, 직급명, 부서코드, 급여, 입사일을 조회
+
+SELECT 
+	EMP_ID, 
+	EMP_NAME, 
+	JOB_NAME, 
+	DEPT_CODE, 
+	SALARY, 
+	HIRE_DATE
+FROM EMPLOYEE
+JOIN JOB USING(JOB_CODE)
+WHERE SALARY = 
+	(SELECT MIN(SALARY) 
+	 FROM EMPLOYEE);
 
                  
 -- 노옹철 사원의 급여보다 많이 받는 직원의 
--- 사번, 이름, 부서, 직급, 급여를 조회
-
+-- 사번, 이름, 부서명, 직급명, 급여를 조회
+SELECT 
+	EMP_ID,
+	EMP_NAME, 
+	DEPT_TITLE,
+	JOB_NAME, 
+	SALARY
+FROM EMPLOYEE
+JOIN JOB USING(JOB_CODE)
+JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID)
+WHERE SALARY >
+	(SELECT SALARY 
+	 FROM EMPLOYEE
+	 WHERE EMP_NAME = '노옹철');
         
+
  
 -- 부서별(부서가 없는 사람 포함) 급여의 합계 중 가장 큰 부서의
 -- 부서명, 급여 합계를 조회 
 
 -- 1) 부서별 급여 합 중 가장 큰값 조회
-
+SELECT MAX(SUM(SALARY))
+FROM EMPLOYEE
+GROUP BY DEPT_CODE; -- D5
 
 
 -- 2) 부서별 급여합이 21760000원 부서의 부서명과 급여 합 조회
+SELECT DEPT_TITLE, SUM(SALARY)
+FROM EMPLOYEE
+LEFT JOIN DEPARTMENT ON(DEPT_CODE = DEPT_ID)
+GROUP BY DEPT_TITLE
+HAVING SUM(SALARY) = 21760000;
 
+/* GROUP BY에 작성된 컬럼명만 SELECT절에 작성할 수 있다!!!  */
 
 
 -- 3) >> 위의 두 서브쿼리 합쳐 부서별 급여 합이 큰 부서의 부서명, 급여 합 조회
+SELECT DEPT_TITLE, SUM(SALARY)
+FROM EMPLOYEE
+LEFT JOIN DEPARTMENT ON(DEPT_CODE = DEPT_ID)
+GROUP BY DEPT_TITLE
+HAVING SUM(SALARY) = 
+	(SELECT MAX(SUM(SALARY))
+	FROM EMPLOYEE
+	GROUP BY DEPT_CODE); -- 서브쿼리 결과 21760000
+                      
+	
+	
+-- 부서별 인원 수가 3명 이상인 부서의 
+-- 부서명, 인원 수 조회
+SELECT DEPT_TITLE, COUNT(*)
+FROM EMPLOYEE
+LEFT JOIN DEPARTMENT 
+	ON (DEPT_CODE = DEPT_ID)
+GROUP BY DEPT_TITLE 
+HAVING COUNT(*) >= 3;
 
-                      
-                      
+
+-- 부서별 인원 수가 가장 적은 부서의 
+-- 부서명, 인원 수 조회
+SELECT
+	NVL(DEPT_TITLE, '없음') "부서명", 
+	COUNT(*)
+FROM EMPLOYEE
+LEFT JOIN DEPARTMENT 
+	ON (DEPT_CODE = DEPT_ID)
+GROUP BY DEPT_TITLE
+HAVING COUNT(*) = 
+	(SELECT MIN(COUNT(*))
+	FROM EMPLOYEE
+	GROUP BY DEPT_CODE); -- 서브쿼리 결과 : 2
+
+
+	
+/***** 서브쿼리에서 사용한 별칭을 메인 쿼리에서 사용하기 *****/
+-- 인라인뷰 : FROM절에 사용된 서브쿼리
+	--> 서브쿼리 결과가 테이블 처럼 인식
+	
+SELECT 이름, 급여
+FROM (SELECT EMP_NAME 이름, SALARY 급여
+	  FROM EMPLOYEE)
+WHERE 급여 >= 4000000;
+	-- 메인쿼리 해석 1순위인 FROM절에 작성된
+	-- 서브쿼리 결과 컬럼명이 "급여"이기 때문에
+
+	-- 메인쿼리 해석 2순위인 WHERE
+	-- 메인쿼리 해석 3순위인 SELECT절에서도
+	-- 똑같이 "급여"라고 컬럼명을 작성해야 한다!!
+	
 
 -------------------------------------------------------------------------
 
