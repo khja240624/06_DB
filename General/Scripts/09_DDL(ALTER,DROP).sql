6bfda553d0215bea11248ddd39ab30ef0522f06b
khja240624 <khja240624@gmail.com>
2024-08-23 16:26:13 +0900

    20240823_DDL, VIEW, SEQUENCE, INDEX, DCL

--- General/Scripts/09_DDL(ALTER,DROP).sql
+++ General/Scripts/09_DDL(ALTER,DROP).sql
@@ -14,36 +14,68 @@
 
 -- * 작성법 중 [] 대괄호 : 생략 가능(선택)
 
--- 제약조건 추가 : ALTER TABLE 테이블명 
---                 ADD [CONSTRAINT 제약조건명] 제약조건(컬럼명) [REFERENCES 테이블명[(컬럼명)]];
+-- 제약조건 추가 : 
+-- ALTER TABLE 테이블명 
+-- ADD [CONSTRAINT 제약조건명] 제약조건(컬럼명) [REFERENCES 테이블명[(컬럼명)]];
 
--- 제약조건 삭제 : ALTER TABLE 테이블명
---                 DROP CONSTRAINT 제약조건명;
+
+-- 제약조건 삭제 : 
+-- ALTER TABLE 테이블명
+-- DROP CONSTRAINT 제약조건명;
 
 
 -- 서브쿼리를 이용해서 DEPARTMENT 테이블 복사(DEPT_COPY) --> NOT NULL 제약조건만 복사됨
+CREATE TABLE DEPT_COPY
+AS SELECT * FROM DEPARTMENT;
+
+SELECT *
+FROM DEPT_COPY;
 
 -- DEPT_COPY 테이블에 PK 추가
+ALTER TABLE DEPT_COPY 
+ADD CONSTRAINT DEPT_COPY_PK PRIMARY KEY(DEPT_ID);
+
 
 -- DEPT_COPY 테이블의 DEPT_TITLE 컬럼에 UNIQUE 제약조건 추가
+-- (DEPT_COPY_TITLE_U)
+ALTER TABLE DEPT_COPY
+ADD CONSTRAINT DEPT_COPY_TITLE_U UNIQUE(DEPT_TITLE);
 
 
 -- DEPT_COPY 테이블의 LOCATION_ID 컬럼에 CHECK 제약조건 추가
 -- 컬럼에 작성할 수 있는 값은 L1, L2, L3, L4, L5 
 -- 제약조건명 : LOCATION_ID_CHK
+ALTER TABLE DEPT_COPY
+ADD CONSTRAINT LOCATION_ID_CHK
+CHECK (LOCATION_ID IN ('L1', 'L2', 'L3', 'L4', 'L5'));
+
 
 
 -- DEPT_COPY 테이블의 DEPT_TITLE 컬럼에 NOT NULL 제약조건 추가
 -- * NOT NULL 제약조건은 다루는 방법이 다름
 -->  NOT NULL을 제외한 제약 조건은 추가적인 조건으로 인식됨.(ADD/DROP)
 -->  NOT NULL은 기존 컬럼의 성질을 변경하는 것으로 인식됨.(MODIFY)
+ALTER TABLE DEPT_COPY
+MODIFY DEPT_TITLE NOT NULL; -- NULL 허용 X
+
+ALTER TABLE DEPT_COPY
+MODIFY DEPT_TITLE NULL; -- NULL 허용
+
+
 
 ---------------------------
 
 -- DEPT_COPY에 추가한 제약조건 중 PK 빼고 모두 삭제
+ALTER TABLE DEPT_COPY
+DROP CONSTRAINT DEPT_COPY_TITLE_U;
+
+ALTER TABLE DEPT_COPY
+DROP CONSTRAINT LOCATION_ID_CHK;
+
 
 -- NOT NULL 제거 시 MODIFY 사용
-
+ALTER TABLE DEPT_COPY
+MODIFY DEPT_TITLE NULL; -- NULL 허용
 
 ---------------------------------------------------------------------------------
 -- 2. 컬럼 추가/수정/삭제
@@ -55,6 +87,8 @@
 --             ALTER TABLE 테이블명 MOIDFY 컬럼명 DEFAULT '값'; (기본값 변경)
 
 --> ** 데이터 타입 수정 시 컬럼에 저장된 데이터 크기 미만으로 변경할 수 없다.
+-- EX) CHAR(3) 짜리 컬럼에 'ABC' 저장된 상태
+--     -> CHAR(1) 수정 하려고하면 오류 발생
 
 
 -- 컬럼 삭제 : ALTER TABLE 테이블명 DROP (삭제할컬럼명);
@@ -66,35 +100,90 @@
 
 
 -- (추가)
--- DEPT_COPY 컬럼에 CNAME VARCHAR2(20) 컬럼 추가
+-- DEPT_COPY 테이블에 CNAME VARCHAR2(20) 컬럼 추가
 -- ALTER TABLE 테이블명 ADD(컬럼명 데이터타입 [DEFAULT '값']);
+ALTER TABLE DEPT_COPY
+ADD(CNAME VARCHAR2(20));
+
+SELECT * FROM DEPT_COPY;
 
 
 -- (추가)
 -- DEPT_COPY 테이블에 LNAME VARCHAR2(30) 기본값 '한국' 컬럼 추가
+ALTER TABLE DEPT_COPY
+ADD (LNAME VARCHAR2(30) DEFAULT '한국');
+
+SELECT * FROM DEPT_COPY;
+-->  새로 추가된 LNAME 컬럼에 기본값 '한국'이 자동으로 세팅됨
 
 
 -- (수정)
--- DEPT_COPY 테이블의 DEPT_ID 컬럼의 데이터 타입을 CHAR(2) -> VARCHAR2(3)으로 변경
+-- DEPT_COPY 테이블의 DEPT_ID 컬럼의 데이터 타입을 
+-- CHAR(2) -> VARCHAR2(3)으로 변경
 -- ALTER TABLE 테이블명 MOIDFY 컬럼명 데이터타입;
+ALTER TABLE DEPT_COPY
+MODIFY DEPT_ID VARCHAR2(3);
+--> 테이블 정보에서 확인
 
 
--- (수정 에러 상황)
+-- (수정 에러 상황 : 저장된 값보다 작은 크기로 변환 불가)
 -- DEPT_TITLE 컬럼의 데이터타입을 VARCHAR2(10)으로 변경
+ALTER TABLE DEPT_COPY
+MODIFY DEPT_TITLE VARCHAR2(10);
+-- ORA-01441: 일부 값이 너무 커서 열 길이를 줄일 수 없음
+
+SELECT 
+	DEPT_TITLE, 
+	LENGTH(DEPT_TITLE)  "문자열 길이", 
+	LENGTHB(DEPT_TITLE) "문자열의 바이트 크기"
+FROM DEPT_COPY;
 
 
 -- (기본값 수정)
 -- LNAME 기본값을 '한국' -> '대한민국' 으로 변경
--- ALTER TABLE 테이블명 MOIDFY 컬럼명 DEFAULT '값'; 
+-- ALTER TABLE 테이블명 MODIFY 컬럼명 DEFAULT '값'; 
+ALTER TABLE DEPT_COPY
+MODIFY LNAME DEFAULT '대한민국';
+
+SELECT * FROM DEPT_COPY;
+
+-- DEPT_COPY 테이블의 LNAME 컬럼 값을
+-- 기본값인 '대한민국'으로 모두 변경
+UPDATE DEPT_COPY
+SET 
+	LNAME = DEFAULT;
+
+SELECT * FROM DEPT_COPY;
+
+COMMIT;
+
 
 
 -- (삭제)
 -- DEPT_COPY에 추가한 컬럼(CNAME, LNAME) 삭제
 -->  ALTER TABLE 테이블명 DROP(삭제할컬럼명);
-
+ALTER TABLE DEPT_COPY
+DROP(CNAME);
 
 -->  ALTER TABLE 테이블명 DROP COLUMN 삭제할컬럼명;
+ALTER TABLE DEPT_COPY
+DROP COLUMN LNAME;
 
+SELECT * FROM DEPT_COPY;
+
+
+-- DEPT_COPY의 모든 컬럼 삭제
+ALTER TABLE DEPT_COPY
+DROP COLUMN LOCATION_ID;
+
+ALTER TABLE DEPT_COPY
+DROP COLUMN DEPT_TITLE;
+
+ALTER TABLE DEPT_COPY
+DROP COLUMN DEPT_ID;
+-- ORA-12983: 테이블에 모든 열들을 삭제할 수 없습니다
+
+SELECT * FROM DEPT_COPY;
 
 
 
@@ -104,6 +193,39 @@
 -- DDL 수행과 동시에 선행 DML이 자동으로 COMMIT 되어버림.
 
 
+SELECT * FROM DEPT_COPY;
+
+-- DEPT_COPY 테이블에 DEPT_ID 컬럼 값으로 'D0' 추가
+-- INSERT(DML) 수행
+INSERT INTO DEPT_COPY
+VALUES('D0'); -- 1행 삽입
+
+SELECT * FROM DEPT_COPY;
+
+-- ROLLBACK을 수행해서 트랜잭션에 저장된 DML 다 지우기
+ROLLBACK;
+
+-- ROLLBACK 결과 확인
+SELECT * FROM DEPT_COPY; -- 'D0' 없어짐
+
+--------------------------------
+/* DML + DDL 혼용 사용하기 */
+INSERT INTO DEPT_COPY
+VALUES('D0'); -- 1행 삽입
+
+SELECT * FROM DEPT_COPY; -- 'D0' 추가됨
+
+-- DEPT_COPY 테이블에 컬럼 추가(DDL 수행)
+ALTER TABLE DEPT_COPY
+ADD (DEPT_TITLE VARCHAR2(30));
+
+SELECT * FROM DEPT_COPY; -- 'DEPT_TITLE' 추가됨
+
+-- ROLLBACK 수행
+ROLLBACK;
+
+SELECT * FROM DEPT_COPY; -- 'D0'가 삭제되지 않음!
+--> ALTER(DDL) 수행 시 트랜잭션이 자동으로 COMMIT됨!
 
 
 --> 결론 : DML/DDL 혼용해서 사용하지 말자!!!
@@ -118,13 +240,44 @@
 
 
 -- 일반 삭제(DEPT_COPY)
+DROP TABLE DEPT_COPY;
 
+SELECT * FROM DEPT_COPY;
+-- 00942: 테이블 또는 뷰가 존재하지 않습니다
 
 
 -- ** 관계가 형성된 테이블 삭제 **
 
+-- TB1 테이블 생성
+CREATE TABLE TB1(
+	TB1_PK NUMBER PRIMARY KEY,
+	TB1_COL NUMBER
+);
+
+-- TB2 테이블 생성(TB1과 관계 설정(FK))
+CREATE TABLE TB2(
+	TB2_PK NUMBER PRIMARY KEY,
+	TB2_COL NUMBER
+		REFERENCES TB1(TB1_PK) 	
+);
 
 
+-- TB1(부모) 테이블 삭제
+DROP TABLE TB1;
+-- ORA-02449: 
+-- 외래 키에 의해 참조되는 고유/기본 키가 
+-- 테이블에 있습니다
+-- ==  PK 컬럼이 다른 테이블에 참조를 당하고 있다
+
+
+-- 해결 방법1) 참조하는 자식 테이블(TB2)을 삭제하거나 FK 제약조건을 삭제
+
+-- 해결 방법2) CASCADE CONSTRAINTS 옵션 사용하기
+--> 테이블 삭제 시 연결되어있는 FK 제약조건을 같이 삭제 시킴
+DROP TABLE TB1 CASCADE CONSTRAINTS;
+-- TB1 테이블 삭제
+
+--   + TB2 FK 제약 조건도 같이 삭제됨!
 
 
 ---------------------------------------------------------------------------------
@@ -132,29 +285,38 @@
 -- 4. 컬럼, 제약조건, 테이블 이름 변경(RENAME)
 
 -- 테이블 복사
-CREATE TABLE DEPT_COPY AS SELECT * FROM DEPARTMENT; 
+CREATE TABLE DEPT_COPY 
+AS SELECT * FROM DEPARTMENT; 
 
 -- 복사한 테이블에 PK 추가
+ALTER TABLE DEPT_COPY
+ADD CONSTRAINT DEPT_COPY_PK PRIMARY KEY(DEPT_ID);
 
+SELECT * FROM DEPT_COPY;
 
 -- 1) 컬럼명 변경 : ALTER TABLE 테이블명 RENAME COLUMN 컬럼명 TO 변경명;
+-- DEPT_TITLE -> DEPT_NAME으로 변경
+ALTER TABLE DEPT_COPY RENAME COLUMN 
+DEPT_TITLE TO DEPT_NAME;
 
+SELECT * FROM DEPT_COPY;
 
 -- 2) 제약조건명 변경 : ALTER TABLE 테이블명 RENAME CONSTRAINT 제약조건명 TO 변경명;
+-- DEPT_COPY_PK -> PK_DCOPY 로 변경
+ALTER TABLE DEPT_COPY RENAME 
+CONSTRAINT DEPT_COPY_PK TO PK_DCOPY;
 
 
 -- 3) 테이블명 변경 : ALTER TABLE 테이블명 RENAME TO 변경명;
+-- DEPT_COPY -> DCOPY 변경
+ALTER TABLE DEPT_COPY RENAME TO DCOPY;
 
 
+SELECT * FROM DEPT_COPY;
+-- ORA-00942: 테이블 또는 뷰가 존재하지 않습니다
 
 
-
-
-
-
-
-
-
+SELECT * FROM DCOPY; -- 변경 확인
 
 
 
--- General/Scripts/10_VIEW, SEQUENCE, INDEX.sql
+++ General/Scripts/10_VIEW, SEQUENCE, INDEX.sql
@@ -23,6 +23,7 @@
  *  [WITH READ OLNY];
  * 
  * 
+ * 
  *  1) OR REPLACE 옵션 : 
  * 		기존에 동일한 이름의 VIEW가 존재하면 이를 변경
  * 		없으면 새로 생성
@@ -42,8 +43,116 @@
 
 
 /* VIEW를 생성하기 위해서는 권한이 필요하다 !!!!*/
+
+-- ORA-01031: 권한이 불충분합니다
+
 -- (관리자 계정 접속)
-GRANT CREATE VIEW TO 계정명;
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+--> 계정명이 언급되는 상황에서는 위에 구문 반드시 실행!!!
+
+-- VIEW 생성 권한 부여
+GRANT CREATE VIEW TO KH_BDH;
+
+-- (KH 계정 접속)
+-- VIEW 생성 구문(기초)
+CREATE VIEW V_EMP
+AS SELECT * FROM EMPLOYEE;
+
+
+-- 사번, 이름, 부서명, 직급명을 쉽게 조회하기 위한 VIEW 생성
+
+-- ORA-00955: 기존의 객체가 이름을 사용하고 있습니다.
+--> OR REPLACE 옵션을 사용해서 해결
+CREATE OR REPLACE VIEW V_EMP 
+AS
+SELECT 
+	EMP_ID 사번,
+	EMP_NAME 이름,
+	NVL(DEPT_TITLE, '없음') 부서명,
+	JOB_NAME 직급명
+FROM EMPLOYEE
+JOIN JOB USING (JOB_CODE)
+LEFT JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID)
+ORDER BY 사번 ASC;
+
+
+-- VIEW를 이용해서 조회하기
+SELECT * FROM V_EMP;
+
+-- V_EMP에서 대리 직원들을 이름 오름차순으로 조회
+-- (VIEW 조회 결과로 보이는 컬럼명을 이용해야된다!)
+SELECT * 
+FROM V_EMP
+WHERE 직급명 = '대리'
+ORDER BY 이름 ASC;
+
+
+------------------------------
+/* VIEW를 이용해서 DML 사용하기 + 문제점 확인 */
+
+-- DEPARTMENT 테이블을 복사한 DEPT_COPY2 생성
+CREATE TABLE DEPT_COPY2
+AS SELECT * FROM DEPARTMENT;
+--> 만들어진 DEPT_COPY2 테이블을 확인해보면
+--  DEPT_TITLE 컬럼만 NULL이 허용되어 있음!
+
+-- DEPT_COPY2 테이블에서 DEPT_ID, LOCATION_ID 컬럼만 이용해서
+-- V_DCOPY2 VIEW 생성
+CREATE OR REPLACE VIEW V_DCOPY2
+AS SELECT DEPT_ID, LOCATION_ID
+	FROM DEPT_COPY2;
+
+
+-- V_DCOPY2 VIEW 생성 확인
+SELECT * FROM V_DCOPY2;
+
+
+-- V_DCOPY2 VIEW를 이용해서 INSERT 수행
+INSERT INTO V_DCOPY2
+VALUES('D0', 'L2');
+--> 가상의 테이블 VIEW인데 INSERT가 성공함!!
+
+-- INSERT 결과 확인 - V_DCOPY2  VIEW
+SELECT * FROM V_DCOPY2;
+-- D0	L2
+
+-- INSERT 결과 확인 - DEPT_COPY2  원본 테이블
+SELECT * FROM DEPT_COPY2;
+-- D0	NULL	L2
+
+-- VIEW에 INSERT를 수행 했지만
+-- VIEW를 만들 때 사용한 원본 테이블에 
+-- 값이 INSERT 됨을 확인!!
+
+  --> 하지만 모든 컬럼 값이 INSERT 된것이 아니라
+  --  VIEW를 생성할 때 사용된 컬럼에만 값이 INSERT되어
+  --  반대로 사용되지 않은 컬럼에는 NULL이 들어감!!
+     -->  NULL은 DB의 무결성을 약하게 만드는 주요 원인!!
+	 --   (가능하면 의도되지 않은 NULL은 존재하지 않게 하자)
+
+
+
+/* WITH READ ONLY 옵션 사용하기*/
+
+-- 왜 사용할까? 
+--> VIEW를 이용해서 DML(INSERT, UPDATE, DELETE) 하지 말라고!
+
+CREATE OR REPLACE VIEW V_DCOPY2
+AS SELECT DEPT_ID, LOCATION_ID
+	FROM DEPT_COPY2
+WITH READ ONLY; -- 읽기 전용
+
+
+-- INSERT 수행
+INSERT INTO V_DCOPY2
+VALUES('D0', 'L3');
+-- ORA-42399: 읽기 전용 뷰에서는 DML 작업을 수행할 수 없습니다.
+
+
+
+
+
+
 
 ----------------------------------------------------------------
 
@@ -64,6 +173,7 @@ GRANT CREATE VIEW TO 계정명;
   [MAXVALUE 숫자 | NOMAXVALUE] -- 발생시킬 최대값 지정 (10의 27승 -1)
   [MINVALUE 숫자 | NOMINVALUE] -- 최소값 지정 (-10의 26승)
   [CYCLE | NOCYCLE] -- 값 순환 여부 지정
+  
   [CACHE 바이트크기 | NOCACHE] -- 캐쉬메모리 기본값은 20바이트, 최소값은 2바이트
 	-- 시퀀스의 캐시 메모리는 할당된 크기만큼 미리 다음 값들을 생성해 저장해둠
 	-- --> 시퀀스 호출 시 미리 저장되어진 값들을 가져와 반환하므로 
@@ -82,9 +192,87 @@ GRANT CREATE VIEW TO 계정명;
  * 						== 마지막으로 호출한 NEXTVAL 값을 반환
  * */
 
+/* 시퀀스 삭제하기 */
+DROP SEQUENCE SEQ_TEST_NO;
+
+
+/* 시퀀스 생성하기 */
+CREATE SEQUENCE SEQ_TEST_NO
+START WITH 100  -- 시작 번호 100
+INCREMENT BY 5  -- NEXTVAL 호출 시 5씩 증가
+MAXVALUE 150    -- 증가 가능한 최대값 150
+NOMINVALUE      -- 최소값 없음
+NOCYCLE         -- 반복 안함
+NOCACHE;        -- 미리 만들어둔 시퀀스 번호 없음
 
 
 
+
+-- 시퀀스 테스트할 테이블 생성
+CREATE TABLE TB_TEST(
+	TEST_NO NUMBER PRIMARY KEY,
+	TEST_NAME VARCHAR2(30) NOT NULL
+);
+
+
+-- 현재 시퀀스 번호 확인하기
+SELECT SEQ_TEST_NO.CURRVAL FROM DUAL;
+--: ORA-08002: 시퀀스 SEQ_TEST_NO.CURRVAL은 
+-- 이 세션에서는 정의 되어 있지 않습니다
+
+--> 발생 원인 : CURRVAL의 정확한 의미는
+--  가장 최근 호출된 NEXTVAL의 값을 반환함을 뜻함
+ --> NEXTVAL를 호출한적이 없어서 오류 발생!!
+
+--> 해결 방법 : NEXTVAL를 호출하면 해결
+SELECT SEQ_TEST_NO.NEXTVAL FROM DUAL; 
+-- 시퀀스 생성 후 첫 NEXTVAL == START WITH 값인 100
+
+
+SELECT SEQ_TEST_NO.CURRVAL FROM DUAL; -- 100
+
+
+-- NEXTVAL를 호출할 때 마다
+-- INCREMENT BY에 작성된 수 만큼 증가
+SELECT SEQ_TEST_NO.NEXTVAL FROM DUAL; 
+-- 처음 : 100
+-- 1회 : 105
+-- 2회 : 110
+-- 3회 : 115
+-- 4회 : 120
+
+
+-- TB_TEST 테이블에 PK 값을 SEQ_TEST_NO 시퀀스로 생성하기
+INSERT INTO TB_TEST
+VALUES(SEQ_TEST_NO.NEXTVAL , '짱구'); -- 125
+
+INSERT INTO TB_TEST
+VALUES(SEQ_TEST_NO.NEXTVAL , '철수'); -- 130
+
+INSERT INTO TB_TEST
+VALUES(SEQ_TEST_NO.NEXTVAL , '유리'); -- 135
+
+SELECT * FROM TB_TEST;
+
+
+--------------
+/* UPDATE에서 시퀀스 사용하기 */
+
+-- '짱구'의 PK 컬럼 값을 
+-- SEQ_TEST_NO 시퀀스의 다음 생성 값으로 변경하기
+UPDATE TB_TEST
+SET
+	TEST_NO = SEQ_TEST_NO.NEXTVAL
+WHERE TEST_NAME = '짱구';
+--> 짱구의 TEST_NO 값을 150까지 증가시키고 나서
+ --> 다시 한번 더 SEQ_TEST_NO.NEXTVAL 호출
+
+-- ORA-08004: 시퀀스 SEQ_TEST_NO.NEXTVAL exceeds 
+-- MAXVALUE은 사례로 될 수 없습니다 
+ --> MAXVALUE 150 보다 증가할 수 없다!!
+
+
+SELECT * FROM TB_TEST;
 
 
 
@@ -92,6 +280,7 @@ GRANT CREATE VIEW TO 계정명;
 --------------------------------
 
 -- SEQUENCE 변경(ALTER)
+--> START WITH 빼고 모두 변경 가능!!
 
 /*
  [작성법]
@@ -103,7 +292,13 @@ GRANT CREATE VIEW TO 계정명;
   [CACHE 바이트크기 | NOCACHE] -- 캐쉬메모리 기본값은 20바이트, 최소값은 2바이트
 */	
 
+-- SEQ_TEST_NO의 MAXVALUE 값을 200으로 수정
+ALTER SEQUENCE SEQ_TEST_NO
+MAXVALUE 200;
 
+-- 200까지 증가시켜서 변경 확인
+SELECT SEQ_TEST_NO.NEXTVAL
+FROM DUAL;
 
 
 
@@ -111,8 +306,11 @@ GRANT CREATE VIEW TO 계정명;
 
 -- VIEW, SEQUENCE 삭제
 
+-- V_DCOPY2 VIEW 삭제
+DROP VIEW V_DCOPY2;
 
-
+-- SEQ_TEST_NO SEQUENCE 삭제
+DROP SEQUENCE SEQ_TEST_NO;
 
 ------------------------------------------------------------------------
 
@@ -123,12 +321,14 @@ GRANT CREATE VIEW TO 계정명;
  * - 인덱스 내부 구조는 B* 트리 형식으로 되어있음.
  * 
  *  
+ * 
  * ** INDEX의 장점 **
- * - 이진 트리 형식으로 구성되어 자동 저렬 및 검색 속도 증가.
+ * - 이진 트리 형식으로 구성되어 자동 정렬 및 검색 속도 증가.
  * 
  * - 조회 시 테이블의 전체 내용을 확인하며 조회하는 것이 아닌
  *   인덱스가 지정된 컬럼만을 이용해서 조회하기 때문에
  *   시스템의 부하가 낮아짐.
+ * 
  * 
  * ** 인덱스의 단점 **
  * - 데이터 변경(INSERT,UPDATE,DELETE) 작업 시 
@@ -152,4 +352,59 @@ GRANT CREATE VIEW TO 계정명;
  *  -> PK 또는 UNIQUE 제약조건이 설정된 컬럼에 대해 
  *    UNIQUE INDEX가 자동 생성된다. 
  * */
+
+/* 인덱스 성능 확인용 테이블 생성 */
+CREATE TABLE TB_IDX_TEST(
+	TEST_NO NUMBER PRIMARY KEY, -- 자동으로 UNIQUE INDEX 생성됨  
+	TEST_ID VARCHAR2(20) NOT NULL
+);
+
+
+/* 관리자 계정 접속 */
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+
+-- 할당된 저장 공간 용량 변경
+ALTER USER KH_BDH
+DEFAULT TABLESPACE USERS
+QUOTA 200M ON USERS;
+
+
+/* 다시 KH 계정 접속 */
+
+-- TB_IDX_TEST 테이블에
+-- 샘플 데이터 100만개 삽입 (PL/SQL 사용)
+BEGIN
+	FOR I IN 1..1000000
+	LOOP 
+		INSERT INTO TB_IDX_TEST
+		VALUES( I , 'TEST' || I);
+	END LOOP;
+	
+	COMMIT;
+END;
+
+
+SELECT COUNT(*) FROM TB_IDX_TEST;
+
+
+/* 인덱스를 사용해서 검색하는 방법!!! 
+ * 	-> WHERE절에 INDEX가 지정된 컬럼을 언급하기
+ * */
+
+/*인덱스 X*/
+-- TEST_ID 가 'TEST500000'인 행 조회하기
+SELECT * FROM TB_IDX_TEST
+WHERE TEST_ID = 'TEST500000'; -- 0.017
+
+/*인덱스 O*/
+-- TEST_NO 가 500000인 행 조회
+SELECT * FROM TB_IDX_TEST
+WHERE TEST_NO = 500000; -- 0.001
+--> 17배 차이 (보통 10~30배 차이)
+
+
+
+
+
+
 
